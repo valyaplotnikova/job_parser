89624e4031e3d5aaf9f72168c0c582599271eef4
valya_plotnikova <valya_ezhova@mail.ru>
2024-05-24 11:59:58 +0300

    appdata jsonSaver

--- src/JSONSaver.py
+++ src/JSONSaver.py
@@ -1,26 +1,42 @@
 import json
 import os
-from src.HeadHunterAPI import HeadHunterAPI
+from abc import ABC, abstractmethod
 
 
-class JSONSaver:
-    def save_to_file(self, vacancies, filename="vacancies.json", directory="data"):
+class FileSaver(ABC):
+    @abstractmethod
+    def save_to_file(self, *args, **kwargs):
+        pass
+
+    def read_to_file(self, *args, **kwargs):
+        pass
+
+    def del_to_file(self, *args, **kwargs):
+        pass
+
+
+class JSONSaver(FileSaver):
+
+    @staticmethod
+    def save_to_file(vacansies, filename="vacancies.json", directory="data"):
         full_path = os.path.join("..", directory, filename)  # Путь к файлу
 
-        for vacancion in vacancies:
+        for vacancy in vacansies:
             with open(full_path, 'w', encoding='utf-8') as file:
-                json.dump(vacancion, file, indent=2, ensure_ascii=False)
+                json.dump(vacancy, file, indent=2, ensure_ascii=False)
 
-    def read_to_file(self, filename="vacancies.json", directory="data"):
+    def read_to_file(self, name_vac, filename="vacancies.json", directory="data"):
         full_path = os.path.join("..", directory, filename)  # Путь к файлу
 
         with open(full_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
-            print(data)
-
+            name_vac_list = []
+            for item in data:
+                if item["name"] == name_vac:
+                    name_vac_list.append(item)
+            return name_vac_list
 
     def del_to_file(self, filename="vacancies.json", directory="data"):
         full_path = os.path.join("..", directory, filename)  # Путь к файлу
         with open(full_path, 'w', encoding='utf-8') as file:
             json.dump("", file, indent=2, ensure_ascii=False)
-
