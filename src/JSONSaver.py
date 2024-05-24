dd784888eee96218b7ea93cb35e4c651cb4cd2fb
valya_plotnikova <valya_ezhova@mail.ru>
2024-05-24 15:50:12 +0300

    appdata jsonsaver 2

--- src/JSONSaver.py
+++ src/JSONSaver.py
@@ -18,23 +18,17 @@ class FileSaver(ABC):
 class JSONSaver(FileSaver):
 
     @staticmethod
-    def save_to_file(vacansies, filename="vacancies.json", directory="data"):
+    def save_to_file(vacancies, filename="vacancies.json", directory="data"):
         full_path = os.path.join("..", directory, filename)  # Путь к файлу
+        with open(full_path, 'w', encoding='utf-8') as file:
+            json.dump(vacancies, file, indent=2, ensure_ascii=False)
 
-        for vacancy in vacansies:
-            with open(full_path, 'w', encoding='utf-8') as file:
-                json.dump(vacancy, file, indent=2, ensure_ascii=False)
-
-    def read_to_file(self, name_vac, filename="vacancies.json", directory="data"):
+    def read_to_file(self, filename="vacancies.json", directory="data"):
         full_path = os.path.join("..", directory, filename)  # Путь к файлу
 
         with open(full_path, 'r', encoding='utf-8') as file:
             data = json.load(file)
-            name_vac_list = []
-            for item in data:
-                if item["name"] == name_vac:
-                    name_vac_list.append(item)
-            return name_vac_list
+            return data
 
     def del_to_file(self, filename="vacancies.json", directory="data"):
         full_path = os.path.join("..", directory, filename)  # Путь к файлу
